51083a88fb6a40091326cb58778c39a4d410b57d
Your Name <chzheng@connect.ust.hk>
2025-02-17 15:33:49 +0800

    Example code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("This is an example code");
     }
 }
